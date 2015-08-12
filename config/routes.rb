bf1d7726555c376e7ef34eeb29c2dce91b45da80
hsuyh <hsuyh82716@gmail.com>
2015-08-12 11:14:05 +0800

    add log in/out in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+
+
   resources :roles
   resources :items
   resources :user_infos
